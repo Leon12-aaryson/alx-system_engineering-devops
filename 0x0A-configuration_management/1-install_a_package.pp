c080731c1929df36c4f90d24993f26b34dadf98b
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-11-24 18:35:53 +0300

    task on installation of flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 # installation of flask version 2.1.0
 
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => '/usr/bin',
-  require => Package['python3-pip'],
+package { 'flask':
+  ensure  => '2.1.0',
+  provider=> 'pip3',
 }
