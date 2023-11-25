7c175e7e19c0e1897dfe80f50cc453e7a3006f0f
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-11-25 18:54:51 +0300

    specifying user on server

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
-file { '/home/your_username/.ssh/config':
-  ensure  => present,
-  owner   => 'your_username',
-  group   => 'your_username',
+file { '/home/ubuntu/.ssh/config':
+  ensure  => 'present',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
   mode    => '0600',
   content => "# SSH client configuration\n\nHost 249537-web-01\n    HostName 100.25.201.247\n    User ubuntu\n    IdentityFile ~/.ssh/school\n    PreferredAuthentications publickey\n    PasswordAuthentication no\n",
 }
